f67de44397f637d21900986933f6f8b20b070603
David Glymph <Woozl@users.noreply.github.com>
2022-08-17 16:42:34 -0400

    nextjs init

---
+++ web/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Quipwit</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
