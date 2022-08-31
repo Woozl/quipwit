67916c5f9b264e2433c455f9ab4a5d4bce31e8d3
David Glymph <Woozl@users.noreply.github.com>
2022-08-30 21:48:09 -0400

    disable certain characters by lowering opacity and setting to grayscale

--- web/components/screens/JoinGame/Character/Character.tsx
+++ web/components/screens/JoinGame/Character/Character.tsx
@@ -4,16 +4,23 @@ interface CharacterProps {
   character: string;
   width?: number;
   height?: number;
+  disabled?: boolean;
 }
 
 const Character = ({
   character,
   width = 128,
   height = 128,
+  disabled = false,
 }: CharacterProps) => {
   return (
     <label>
-      <input type="radio" name="character" value={character} />
+      <input
+        type="radio"
+        name="character"
+        value={character}
+        disabled={disabled}
+      />
       <Image
         src={`/characters/${character}.svg`}
         width={width}
